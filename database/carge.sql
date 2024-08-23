3079b67fd96627eea142a6e5828e31db8389f577
Simone <74455567+Simone-Merengone@users.noreply.github.com>
2024-08-23 17:34:55 +0200

    Update carge.sql

--- database/carge.sql
+++ database/carge.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: localhost
--- Creato il: Ago 23, 2024 alle 17:15
+-- Creato il: Ago 23, 2024 alle 17:34
 -- Versione del server: 8.0.39-0ubuntu0.20.04.1
 -- Versione PHP: 8.1.29
 
@@ -20,6 +20,39 @@ SET time_zone = "+00:00";
 --
 -- Database: `S4984409`
 --
+
+-- --------------------------------------------------------
+
+--
+-- Struttura della tabella `car`
+--
+
+CREATE TABLE `car` (
+  `id` int NOT NULL,
+  `manufacturer` varchar(50) COLLATE utf8mb4_general_ci NOT NULL,
+  `model` varchar(50) COLLATE utf8mb4_general_ci NOT NULL,
+  `price` float NOT NULL,
+  `year` int NOT NULL,
+  `hp` smallint NOT NULL,
+  `fuel` enum('gasoline','diesel','electric','hybrid') COLLATE utf8mb4_general_ci NOT NULL,
+  `gear` enum('automatic','manual','semi-automatic') COLLATE utf8mb4_general_ci NOT NULL,
+  `color` enum('black','grey','white','red','green','orange','yellow') COLLATE utf8mb4_general_ci NOT NULL,
+  `description` varchar(100) COLLATE utf8mb4_general_ci DEFAULT NULL,
+  `img` varchar(100) COLLATE utf8mb4_general_ci NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Struttura della tabella `invoice`
+--
+
+CREATE TABLE `invoice` (
+  `id` int NOT NULL,
+  `document` varchar(255) COLLATE utf8mb4_general_ci NOT NULL,
+  `user_id` int DEFAULT NULL,
+  `date` date NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 -- --------------------------------------------------------
 
@@ -55,6 +88,20 @@ INSERT INTO `user` (`id`, `firstname`, `lastname`, `email`, `type`, `pass`, `ban
 --
 
 --
+-- Indici per le tabelle `car`
+--
+ALTER TABLE `car`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indici per le tabelle `invoice`
+--
+ALTER TABLE `invoice`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `document` (`document`),
+  ADD KEY `user_id` (`user_id`);
+
+--
 -- Indici per le tabelle `user`
 --
 ALTER TABLE `user`
@@ -68,10 +115,32 @@ ALTER TABLE `user`
 --
 
 --
+-- AUTO_INCREMENT per la tabella `car`
+--
+ALTER TABLE `car`
+  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=21;
+
+--
+-- AUTO_INCREMENT per la tabella `invoice`
+--
+ALTER TABLE `invoice`
+  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=22;
+
+--
 -- AUTO_INCREMENT per la tabella `user`
 --
 ALTER TABLE `user`
   MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=63;
+
+--
+-- Limiti per le tabelle scaricate
+--
+
+--
+-- Limiti per la tabella `invoice`
+--
+ALTER TABLE `invoice`
+  ADD CONSTRAINT `fk_invoice_user` FOREIGN KEY (`user_id`) REFERENCES `user` (`id`) ON DELETE CASCADE;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
